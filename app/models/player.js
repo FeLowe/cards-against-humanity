f9ac42dea573f93f939f1126db176845da93731e
Fernanda Lowe <fernanda@>
2016-06-20 09:48:26 -0700

    add models, index and rules route

---
+++ app/models/player.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+// import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default DS.Model.extend({
+  username: DS.attr(),
+  userage: DS.attr(),
+  userpoints: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
+});
