fea33a937f84a31117c59080f116caee4bf27c50
john <jldjld503@gmail.com>
2016-06-20 11:34:47 -0700

    added form for player submission. simple functionality, easily broken but will work for testing purposes. hopefully will replace in future with game model boolean for form showing and game reset will change boolean

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('rules');
+  this.route('player');
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('player');
+  },
+  actions: {
+    submitPlayers(params1, params2, params3, params4) {
+      var player1 = this.store.createRecord('player', params1);
+      var player2 = this.store.createRecord('player', params2);
+      var player3 = this.store.createRecord('player', params3);
+      var player4 = this.store.createRecord('player', params4);
+      player1.save();
+      player2.save();
+      player3.save();
+      player4.save();
+      this.transitionTo('index');
+      $('#submitPlayers').hide();
+    },
+  },
 });
