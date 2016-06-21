d3ddbbf3bed8f27ba3dcf6d141b3fcb7fcc0cd91
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-06-21 09:12:25 -0700

    create and delete 10 card hands but no assignment to player yet

--- app/components/delete-players/component.js
+++ app/components/delete-players/component.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   actions: {
-    deletePlayers(players) {
+    deletePlayers(players, answers) {
+      var answerArray = answers.toArray();
+      console.log(answerArray);
+      answerArray.forEach(function(answer) {
+        answer.set('drawn', "false");
+        answer.save();
+      });
       this.sendAction('deletePlayers', players);
     },
   }
--- app/components/submit-players/component.js
+++ app/components/submit-players/component.js
@@ -1,55 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  answersDealt: Ember.computed(function() {
-    var allAnswers = [];
-    var answerHand = [];
-
-    this.get('answers').forEach(function(answer) {
-      allAnswers.push(answer);
-    });
-
-    // for (var i=0; i < 10; i++) {
-    //   var rand = Math.floor(Math.random() * allAnswers.length);
-    //   // while (allAnswers[rand].drawn) {
-    //   //   rand = Math.floor(Math.random() * allAnswers.length);
-    //   // }
-    //   if (allAnswers[rand].get('drawn') === false) {
-    //     allAnswers[rand].set('drawn', true);
-    //     answerHand.push(allAnswers[rand]);
-    //   } else {
-    //     // --i;
-    //     rand = Math.floor(Math.random() * allAnswers.length);
-    //   }
-    // }
-    // while (answerHand.length < 10) {
-    //   var rand = Math.floor(Math.random() * allAnswers.length);
-    //
-    //   if (allAnswers[rand].get('drawn') === false) {
-    //     allAnswers[rand].set('drawn', true);
-    //     answerHand.push(allAnswers[rand]);
-    //   } else {
-    //     // --i;
-    //     rand = Math.floor(Math.random() * allAnswers.length);
-    //   }
-    // }
-
-    var i = 0;
-    while (i < 10) {
-      var rand = Math.floor(Math.random() * allAnswers.length);
-      if (allAnswers[rand].get('drawn') === "false") {
-        allAnswers[rand].set('drawn', "true");
-        console.log(allAnswers[rand].get('drawn'));
-        answerHand.push(allAnswers[rand]);
-        i++;
-      } else {
-        rand = Math.floor(Math.random() * allAnswers.length);
-      }
-    }
-
-    console.log(answerHand);
-    return answerHand;
-  }),
+  // answersDealt: Ember.computed(function() {
+  //   var allAnswers = [];
+  //   var answerHand = [];
+  //
+  //   this.get('answers').forEach(function(answer) {
+  //     allAnswers.push(answer);
+  //   });
+  //
+  //   while (answerHand.length < 10) {
+  //     var rand = Math.floor(Math.random() * allAnswers.length);
+  //     if (allAnswers[rand].get('drawn') === "false") {
+  //       allAnswers[rand].set('drawn', "true");
+  //       console.log(allAnswers[rand].get('drawn'));
+  //       answerHand.push(allAnswers[rand]);
+  //     } else {
+  //       rand = Math.floor(Math.random() * allAnswers.length);
+  //     }
+  //   }
+  //
+  //   console.log(answerHand);
+  //   return answerHand;
+  // }),
   actions: {
     submitPlayers() {
       var params1 = {
@@ -72,7 +45,7 @@ export default Ember.Component.extend({
         userage: "this.get('userage4')",
         userpoints: 0,
       };
-      this.sendAction('submitPlayers', params1, params2, params3, params4);
+      this.sendAction('submitPlayers', params1, params2, params3, params4, this.get('answers'));
       this.set('username1', '');
       this.set('userage1', '');
       this.set('username2', '');
@@ -81,7 +54,6 @@ export default Ember.Component.extend({
       this.set('userage3', '');
       this.set('username4', '');
       this.set('userage4', '');
-      console.log(this.get('answersDealt'));
     },
   }
 });
